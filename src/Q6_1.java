1a3e8178041acadd43d8c79dc2e74fcbbb989301
justinzwd <1462724765@qq.com>
2020-10-25 10:25:45 +0800

    面试题6 2.0

--- src/Q6_1.java
+++ src/Q6_1.java
@@ -1,22 +1,18 @@
 import java.util.Stack;
 
-public class Q6
+public class Q6_1
 {
     //只打印链表，不反转链表
     public void reversePrint(LinkedList1 head)
     {
-        Stack<Integer> stack = new Stack<>();
-
         LinkedList1 temp = head;
-        while (temp != null)
+        if (temp != null)
         {
-            stack.push(temp.data);
-            temp = temp.next;
-        }
-
-        while (!stack.isEmpty())
-        {
-            System.out.println(stack.pop());
+            if (temp.next != null)
+            {
+                reversePrint(temp.next);
+            }
+            System.out.println(temp.data);
         }
     }
 
@@ -25,23 +21,26 @@ public class Q6
         LinkedList1 node1 = new LinkedList1(1);
         LinkedList1 node2 = new LinkedList1(2, node1);
         LinkedList1 node3 = new LinkedList1(3, node2);
+
+        //注意这里node3是head
         new Q6().reversePrint(node3);
     }
 }
 
-class LinkedList
+class LinkedList1
 {
     public int data;
     public LinkedList1 next;
 
-    public LinkedList(int data, LinkedList1 next)
+    public LinkedList1(int data, LinkedList1 next)
     {
         this.data = data;
         this.next = next;
     }
 
-    public LinkedList(int data)
+    public LinkedList1(int data)
     {
         this.data = data;
     }
 }
+
